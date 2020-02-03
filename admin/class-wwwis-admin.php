4c65f2b6eea327eb6afdfb217c25992457803ece
Wordpress-Webmaster <60612681+Wordpress-Webmaster@users.noreply.github.com>
2020-02-03 15:43:07 +0100

    Update class-wwwis-admin.php

--- admin/class-wwwis-admin.php
+++ admin/class-wwwis-admin.php
@@ -73,7 +73,7 @@ class Wwwis_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wwwis-admin.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'wwwis-admin.css', array(), $this->version, 'all' );
 
 	}
 
@@ -96,7 +96,7 @@ class Wwwis_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wwwis-admin.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'wwwis-admin.js', array( 'jquery' ), $this->version, false );
 
 	}
 
