b5b26fcfc91a3241eeeb0c6f913edad49662676e
Wordpress-Webmaster <60612681+Wordpress-Webmaster@users.noreply.github.com>
2020-02-03 15:38:14 +0100

    Update class-wwwis-deactivator.php

--- includes/class-wwwis-deactivator.php
+++ includes/class-wwwis-deactivator.php
@@ -5,32 +5,11 @@
  *
  * @link       https://wordpress-webmaster.de
  * @since      1.0.0
- *
- * @package    Wwwis
- * @subpackage Wwwis/includes
- */
-
-/**
- * Fired during plugin deactivation.
- *
- * This class defines all code necessary to run during the plugin's deactivation.
- *
- * @since      1.0.0
  * @package    Wwwis
  * @subpackage Wwwis/includes
  * @author     Volkan Sah <plugin@wordpress-webmaster.de>
  */
 class Wwwis_Deactivator {
-
-	/**
-	 * Short Description. (use period)
-	 *
-	 * Long Description.
-	 *
-	 * @since    1.0.0
-	 */
 	public static function deactivate() {
-
 	}
-
 }
