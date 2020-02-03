d5eee30dbf5b3569925b4901b7f0402e581753ec
Wordpress-Webmaster <60612681+Wordpress-Webmaster@users.noreply.github.com>
2020-02-03 15:37:33 +0100

    Update class-wwwis-activator.php

--- includes/class-wwwis-activator.php
+++ includes/class-wwwis-activator.php
@@ -5,32 +5,12 @@
  *
  * @link       https://wordpress-webmaster.de
  * @since      1.0.0
- *
- * @package    Wwwis
- * @subpackage Wwwis/includes
- */
-
-/**
- * Fired during plugin activation.
- *
- * This class defines all code necessary to run during the plugin's activation.
- *
- * @since      1.0.0
  * @package    Wwwis
  * @subpackage Wwwis/includes
  * @author     Volkan Sah <plugin@wordpress-webmaster.de>
  */
 class Wwwis_Activator {
-
-	/**
-	 * Short Description. (use period)
-	 *
-	 * Long Description.
-	 *
-	 * @since    1.0.0
-	 */
 	public static function activate() {
-
 	}
 
 }
